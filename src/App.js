a1c2365af11f8c7c2176ae1250e1c13f1b07cc85
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-04-10 19:43:17 -0400

    added github repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Adessa Morciglio and is
+      </footer>
+      <a
+        href="https://github.com/Cute-Bologna/reactweatherappweek5.git"
+        rel="norefferrer"
+        target="_blank"
+      >
+        open sourced on GitHub
+      </a>
+    </div>
+  );
 }
 
 
