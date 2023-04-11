697ae1087954b956b913a4cec26b0d36612389d3
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-04-10 20:19:48 -0400

    added weather componant and css file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
@@ -8,7 +9,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
-      
+      <Weather />
       <footer>
         This project was coded by Adessa Morciglio and is{" "}
       <a
