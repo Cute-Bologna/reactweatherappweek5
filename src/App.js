fd09cc2f48a2be26532ff9bb0803712e0607edd6
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-04-10 20:08:36 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+
+import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather App</h1>
+      
       <footer>
-        This project was coded by Adessa Morciglio and is
-      </footer>
+        This project was coded by Adessa Morciglio and is{" "}
       <a
         href="https://github.com/Cute-Bologna/reactweatherappweek5.git"
         rel="noreferrer"
@@ -15,6 +18,8 @@ export default function App() {
       >
         open sourced on GitHub
       </a>
+      </footer>
+    </div>
     </div>
   );
 }
