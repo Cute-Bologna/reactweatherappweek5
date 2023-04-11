546fbf265c7c7ea3f7364623fc4771c1a26c0e94
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-04-10 20:51:30 -0400

    It's not aamzing but it's foundation

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,47 @@
 import React from "react";
 import "./Weather.css";
 
+
 export default function Weather() {
-    return <div className="Weather">Hello from Weather</div>;
+    return <div className="Weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+            <input type="search" placeholder="Enter a city.." className="form-control"/>
+            </div>
+            <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+            </div>
+
+        </form>
+        <h1> Beirut</h1>
+        <ul>
+            <li>
+                Wednesday 07:00
+            </li>
+            <li>Mostly Cloudy</li>
+            
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img src="*" alt="mostlycloudy"/>
+                6&deg;C
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>
+                            percipitation: 15%
+                        </li>
+                        <li>
+                            Humidity: 69%
+                        </li>
+                        <li>
+                            Windspeed: 9mph
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
 
 }
